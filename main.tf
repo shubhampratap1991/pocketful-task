6b5fb715c4bc23741247d8dd0b16ad0334c4bdb0
shubhampratap1991 <42820703+shubhampratap1991@users.noreply.github.com>
2024-01-02 20:29:11 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 resource "aws_instance" "web" {
   ami                    = var.ami_id
   instance_type          = var.instance_type
-  security_groups        = var.sg  
+  security_groups        = [var.sg]  
   subnet_id              = var.subnet_id
-  vpc_security_group_ids = var.vpc_security_group_ids 
+  vpc_security_group_ids = [var.vpc_security_group_ids]
   key_name               = var.key_name
 
   tags = {
