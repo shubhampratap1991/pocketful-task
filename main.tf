570f0c6f1f3439eb4a2f0f71059688eb6906e48b
shubhampratap1991 <42820703+shubhampratap1991@users.noreply.github.com>
2024-01-02 06:52:09 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,6 +4,7 @@ resource "aws_instance" "web" {
   subnet_id              = var.subnet_id
   vpc_security_group_ids = [var.vpc_security_group_ids]
   key_name               = var.key_name
+  placement_group                      = var.placement_group
 
   tags = {
     Name = var.instance_name
